e1dfa31c4b9104ab33adc6a5b53ce6bc252209e5
aarigato <ayruun@icloud.com>
2019-06-28 21:09:52 +0200

    add decrypt component

--- src/components/TabDecrypt.vue
+++ src/components/TabDecrypt.vue
@@ -1,14 +1,69 @@
 <template>
   <div class="tab-decrypt">
-    <span>TabDecrypt</span>
+    <p class="headlines">Encryption Key:</p>
+    <input v-model="inputKey" placeholder="Enter your Key">
+
+    <p class="headlines">Enter the Message:</p>
+    <input v-model="inputMessage" placeholder="Enter your Message">
+    <button @click="pasteInput">Paste</button>
+
+    <p class="headlines">Decrypted:</p>
+    <input :value="decrypt(inputMessage)" disabled>
+    <input type="hidden" id="encrypted-msg" :value="decryptedMessage">
+    
   </div>
 </template>
 
 <script>
 export default {
-  name: "TabDecrypt"
+  name: "TabDecrypt",
+  data() {
+    return {
+      inputKey: "",
+      inputMessage: "",
+      decryptedMessage: ""
+    };
+  },
+  methods: {
+    decrypt(input) {
+      if (!parseInt(this.inputKey)) {
+        return "PLEASE ENTER YOUR KEY!";
+      } else if (parseInt(this.inputKey) > 9999) {
+        return "Key has to be lower than 9999";
+      }
+
+      this.decryptedMessage = input
+        .split("")
+        .map(el => {
+          let charCode = el.charCodeAt() - parseInt(this.inputKey);
+          return String.fromCharCode(charCode);
+        })
+        .join("");
+
+      return this.decryptedMessage;
+    },
+    pasteInput() {
+      console.log("paste");
+    }
+  }
 };
 </script>
 
 <style>
+.tab-decrypt {
+  height: 50vh;
+  width: 50vw;
+  background: grey;
+  padding: 25px;
+}
+
+.headlines {
+  font-size: 1.2em;
+  margin: 5px;
+}
+
+input {
+  margin-bottom: 25px;
+  width: 100%;
+}
 </style>
